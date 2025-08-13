17e57ba51c1ea53bb87b16bef02c67fbf3c07025
felipe-1945 <felipe.mendes.batista@escola.pr.gov.br>
2025-08-13 16:29:09 -0300

    Create perguntas.js

---
+++ assets/perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Baixo Tagima',
+    'Qual é a principal função do baixo em uma banda?',
+    'Manter a base rítmica e harmônica.'
+)
+
+criaCartao(
+    'Baixo Tagima',
+    'Qual é a origem da marca Tagima?',
+    'Marca brasileira fundada por Seizi Tagima.'
+)
+
+criaCartao(
+    'Baixo Tagima',
+    'Qual é o material mais comum usado no corpo de um baixo Tagima?',
+    'Normalmente marupá ou basswood.'
+)
+
+criaCartao(
+    'Baixo Tagima',
+    'O que significa escala do baixo?',
+    'Distância entre a pestana e a ponte do instrumento.'
+)
+
+criaCartao(
+    'Baixo Tagima',
+    'Qual a diferença entre baixo ativo e passivo?',
+    'Baixo ativo tem circuito eletrônico; passivo não.'
+)
+
+criaCartao(
+    'Baixo Tagima',
+    'O que é um captador no baixo?',
+    'Dispositivo que converte vibrações das cordas em sinal elétrico.'
+)
+
+criaCartao(
+    'Baixo Tagima',
+    'Quantas cordas possui um baixo padrão?',
+    'Geralmente 4 cordas, mas pode ter 5 ou 6.'
+)
+
+criaCartao(
+    'Baixo Tagima',
+    'Qual é o modelo mais famoso da Tagima?',
+    'Tagima Jazz Bass (TJB) e Tagima Millenium.'
+)
+
+criaCartao(
+    'Baixo Tagima',
+    'O que é traste no baixo?',
+    'Pequenas divisões no braço que ajudam na afinação.'
+)
+
+criaCartao(
+    'Baixo Tagima',
+    'Qual é a afinação padrão de um baixo de 4 cordas?',
+    'E - A - D - G.'
+)
